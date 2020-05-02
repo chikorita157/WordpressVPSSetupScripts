03fb52500f1f009014b088058248de1cf98c7124
香風智乃 <koufuuchino@koufuuchinos-MacBook-puro.local>
2020-05-02 18:18:55 -0400

    The final script

--- lempsetup.sh
+++ lempsetup.sh
@@ -3,30 +3,33 @@ if [ "$(id -u)" != "0" ]; then
 	echo "You must be root to run this script"
 		exit 1
 else
+	echo "Disabiling Password Authentication"
+	# Disable password authentication
+	sudo grep -q "ChallengeResponseAuthentication" /etc/ssh/sshd_config && sed -i "/^[^#]*ChallengeResponseAuthentication[[:space:]]yes.*/c\ChallengeResponseAuthentication no" /etc/ssh/sshd_config || echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
+	sudo grep -q "^[^#]*PasswordAuthentication" /etc/ssh/sshd_config && sed -i "/^[^#]*PasswordAuthentication[[:space:]]yes/c\PasswordAuthentication no" /etc/ssh/sshd_config || echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+	sudo service ssh restart
+	
+	apt update
 	echo "Installing required packages"
-	dnf install wget unzip -y
+	apt install wget unzip -y
 	echo "Setting up nginx"
-	dnf install nginx -y
+	apt install nginx -y
 	systemctl enable nginx
-	systemctl start nginx
-	firewall-cmd --permanent --add-service=http
-	firewall-cmd --permanent --zone=public --add-service=https
-	firewall-cmd --reload
+	service nginx start
+	ufw allow 'Nginx Full'
 	
 	echo "Setting up MariaDB"
-	dnf install mariadb-server -y
-	sudo systemctl enable mariadb
-	systemctl start mariadb
+	apt install mysql-server -y
+	sudo systemctl enable mysql
+	service mysql start
 	mysql_secure_installation
 	
 	echo "setting up PHP"
-	dnf install php-fpm php-mysqlnd php-json php-gd php-xml php-mbstring tar curl -y
-	wget https://raw.githubusercontent.com/chikorita157/WordpressVPSSetupScripts/master/www.conf?token=AABHLH37FBXZZ5DQYTJ7NBC6VXEGO
-	mv www.conf /etc/php-fpm.d
-	sudo systemctl enable php-fpm
-	systemctl start php-fpm
+	apt install php-fpm php-mysql php-json php-gd php-xml php-mbstring tar curl -y
+	systemctl enable php7.4-fpm
+	service php7.4-fpm start
 	
 	echo "Restarting nginx"
-
+	systemctl restart nginx
 	exit 0
 fi
